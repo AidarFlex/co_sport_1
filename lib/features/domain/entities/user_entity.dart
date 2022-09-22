348ca5f7829c23f41f3a320817af0444a99ed4df
Айдар Мавлетбаев <mavletaidar@gmail.com>
2022-09-23 00:12:07 +0300

    Update user_entity.dart

--- lib/features/domain/entities/user_entity.dart
+++ lib/features/domain/entities/user_entity.dart
@@ -3,10 +3,26 @@ import 'package:equatable/equatable.dart';
 class UserEntity extends Equatable {
   final String userId;
   final String username;
+  final String fullName;
+  final String profileImage;
+  final String location;
+  final String bio;
 
-  const UserEntity({required this.userId, required this.username});
+  const UserEntity(
+      {required this.userId,
+      required this.username,
+      required this.fullName,
+      required this.profileImage,
+      required this.location,
+      required this.bio});
 
   @override
-  // TODO: implement props
-  List<Object?> get props => throw UnimplementedError();
+  List<Object?> get props => [
+        userId,
+        username,
+        fullName,
+        profileImage,
+        location,
+        bio,
+      ];
 }
